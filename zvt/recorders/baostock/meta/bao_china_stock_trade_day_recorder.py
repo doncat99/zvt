14d0a02762e55078e25d6c8f90903b110b3f7595
Don Wong <doncat99@gmail.com>
2021-03-09 14:15:16 +0800

    sync project

--- zvt/recorders/baostock/meta/bao_china_stock_trade_day_recorder.py
+++ zvt/recorders/baostock/meta/bao_china_stock_trade_day_recorder.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 import pandas as pd
 
 from zvt.api.data_type import Region, Provider
 from zvt.domain import StockTradeDay, Stock
 from zvt.contract.recorder import RecorderForEntities
+from zvt.contract.api import df_to_db
 from zvt.networking.request import bao_get_trade_days
 from zvt.utils.time_utils import to_time_str, PD_TIME_FORMAT_DAY
+from zvt.utils.pd_utils import pd_is_not_null
 
 
 class BaoChinaStockTradeDayRecorder(RecorderForEntities):
@@ -26,14 +30,28 @@ class BaoChinaStockTradeDayRecorder(RecorderForEntities):
             start = to_time_str(trade_days['timestamp'].max(axis=0))
         else:
             start = "1990-12-19"
-        return bao_get_trade_days(start_date=start)
+        df = bao_get_trade_days(start_date=start)
+
+        if pd_is_not_null(df):
+            df = self.format(entity, df)
+            self.persist(df)
+
+        return None
 
     def format(self, entity, df):
-        df['timestamp'] = pd.to_datetime(df[df['is_trading_day'] == '1']['calendar_date'])
+        dates = df[df['is_trading_day'] == '1']['calendar_date'].values
+        df = pd.DataFrame(dates, columns=['timestamp'])
+
+        if not isinstance(df['timestamp'].dtypes, datetime):
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+
         df['entity_id'] = 'stock_sz_000001'
         df['provider'] = self.provider.value
         df['id'] = self.generate_domain_id(entity, df)
         return df
+
+    def persist(self, df):
+        df_to_db(df=df, ref_df=None, region=self.region, data_schema=self.data_schema, provider=self.provider)
 
     def on_finish(self):
         pass
